b39e534a8871122d88f6e5fb35ce1ac27518b2e3
darkkennn <sacpan2000@gmail.com>
2024-02-25 23:02:35 +0530

    Practicing fetch more

--- Learn/learning js/Fetch API sandbox/fetch.js
+++ Learn/learning js/Fetch API sandbox/fetch.js
@@ -1,10 +1,27 @@
 button = document.getElementById('getText');
 
 button.addEventListener('click', getText);
+button.addEventListener('click', getUsers);
 
 function getText() {
     fetch('sample.txt')
     .then((res) => res.text())
-    .then((data) => console.log(data)) 
+    .then((data) => {
+        document.getElementById('output').innerHTML = data;
+    }) 
 }
 
+function getUsers() {
+    fetch('users.json')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2> Users: </h2>';
+        data.forEach(function(user){
+            output += `
+                <ul>
+                    <li> </li>
+                </ul>
+            `
+        })
+    })
+}
