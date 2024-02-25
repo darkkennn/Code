16e2a0c5558659e8c1e9ab514f8bb4b1c25ac814
darkkennn <sacpan2000@gmail.com>
2024-02-26 03:10:55 +0530

    Todo list updates

---
+++ Learn/learning ts/todo list/todo.js
@@ -0,0 +1,40 @@
+var span = document.getElementById('span');
+
+function time() {
+  let d = new Date();
+  let s = d.getSeconds();
+  let m = d.getMinutes();
+  let h = d.getHours();
+  span.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+
+setInterval(time, 1000);
+
+const myForm = document.getElementById('my-form');
+const msg = document.querySelector('.msg');
+const date = document.getElementById('date');
+const task = document.getElementById('task');
+const list = document.getElementById('list');
+
+myForm.addEventListener('submit', onSubmit)
+
+function onSubmit(){
+    e.preventDefault();
+
+    if(date.value == '' || task.value ==''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(),3000)
+    }
+    else{
+        const li =  document.createElement('li');
+        li.appendChild(document.createTextNode(`${task.value} : ${date.value}`));
+
+        list.appendChild(li);
+        
+        date.value = '';
+        task.value = '';
+    }
+}
