d88171e7e20a1fb0e0e5fe6a3e7627b6685421da
darkkennn <sacpan2000@gmail.com>
2024-02-26 16:26:19 +0530

    Adding the remove button

--- Learn/LearningTS/todo list/todo.js
+++ Learn/LearningTS/todo list/todo.js
@@ -1,9 +1,11 @@
 var span = document.getElementById('span');
 const myForm = document.getElementById('my-form');
-const msg = document.querySelector('.msg');
+const msg = document.getElementById('msg');
 const date = document.getElementById('date');
 const task = document.getElementById('task');
 const list = document.getElementById('list');
+const btn = document.getElementById('btn');
+const button = document.getElementById('button');
 
 function time() {
   let d = new Date();
@@ -16,9 +18,9 @@ function time() {
 
 setInterval(time, 1000);
 
-myForm.addEventListener('submit', onSubmit)
+btn.addEventListener('click', onClick)
 
-function onSubmit(){
+function onClick(e){
     e.preventDefault();
 
     if(date.value == '' || task.value ==''){
@@ -32,7 +34,18 @@ function onSubmit(){
         li.appendChild(document.createTextNode(`${task.value} : ${date.value}`));
 
         list.appendChild(li);
+
+        button.addEventListener('click', onPress);
         
+        ul = document.getElementById('ul');
+
+        function onPress(e){
+        e.preventDefault();
+
+        li.removeChild(li.lastChild)
+     }
+
+        //resetting the values
         date.value = '';
         task.value = '';
     }
