b783c11724850d3ef97922ceb02700832d6f681f
darkkennn <sacpan2000@gmail.com>
2024-02-27 20:00:00 +0530

    sorry

--- Learn/learningReact/basics/src/App.js
+++ Learn/learningReact/basics/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see ok
+          Edit <code>src/App.js</code> and save to see whats up
         </p>
         <a
           className="App-link"
