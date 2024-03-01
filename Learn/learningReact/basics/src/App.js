fb415e083033882b35085f068a6b927271f2bca5
darkkennn <sacpan2000@gmail.com>
2024-03-01 22:36:09 +0530

    fixing the app.js in learning react

--- Learn/learningReact/basics/src/App.js
+++ Learn/learningReact/basics/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see whats up
+          Edit <code>src/App.js</code> and save to see whats changing
         </p>
         <a
           className="App-link"
