1cd55efc1ab9bb89409cd1df83a0e74aba804c14
darkkennn <sacpan2000@gmail.com>
2024-03-15 15:38:24 +0530

    creating a page with changing names using react

--- Learn/learningReact/basics/src/App.js
+++ Learn/learningReact/basics/src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = 'Sachin';
-
+  const handleNameChanges = () => {
+    const names = ['Sachin', 'Kevin', 'Goofballer'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -17,3 +21,4 @@ function App() {
 }
 
 export default App;
+ 
