b0dc20fe89081f78c2f94bb349138819ef615398
darkkennn <sacpan2000@gmail.com>
2024-03-16 15:06:43 +0530

    installing react icons and updating the app to learn set

--- Learn/learningReact/basics/src/Content.js
+++ Learn/learningReact/basics/src/Content.js
@@ -1,22 +1,39 @@
+import { useState } from "react";
+
 const Content = () => {
-    const handClick = () => {
-        console.log('You clicked the button');
-    }
-    const handleNameChanges = () => {
-        const names = ['Sachin', 'Kevin', 'Goofballer'];
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
-      }
+    const (items, setItems) = useState([
+        {
+            id: 1,
+            checked: false,
+            items: "Cocoa powder"
+        },
+        {
+            id:2,
+            checked:false,
+            items:"Bag of beans"
+        },
+        {
+            id:3,
+            checked:false,
+            items:"Protein powder"
+        }
+    ]);
 
-  return (
-    <main>
-
-        <p>
-            Hello {handleNameChanges()}!
-        </p>
-        <button onClick = {handClick()}>Click</button>
-    </main>
+    return (
+        <main>
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key = {item.id}>
+                        <input 
+                        type = "checkbox"
+                        checked = {item.checked}
+                        />
+                        <label>{item.item}</label>
+                        <button>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </main>
     )
-}
 
-export default Content
+}
